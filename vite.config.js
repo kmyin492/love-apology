c89d69f74c065f7a7dd880e9501de1de1425d618
Khin Maung Yin <khinmaungyin@agbcommunication.com>
2026-04-24 18:39:43 +0630

    whitescreen

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   base: "/love-apology/",
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite"; // Tailwind v4 သုံးထားတာမို့ ဒါလေးပါနိုင်ပါတယ်
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/love-apology/", // <--- ဒီစာကြောင်းကို အတိအကျ ထည့်ပေးပါ
 });
